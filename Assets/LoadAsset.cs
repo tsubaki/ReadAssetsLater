7a2d51a750b400a925174f019dc03fd5463b7138
tsubaki <ruitomo.ex@gmail.com>
2016-12-27 14:04:26 +0900

    first commit

---
+++ Assets/LoadAsset.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadAsset : MonoBehaviour {
+
+    string status = string.Empty;
+
+	void Start () {
+        AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/scene");
+        SceneManager.LoadScene("stage", LoadSceneMode.Additive);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("stage"));
+	}
+
+    public void LoadPlayerAssetBundle()
+    {
+        StartCoroutine(LoadAssetCoroutine("player", "Player"));
+    }
+
+    public void LoadBallAssetBundle()
+    {
+        StartCoroutine(LoadAssetCoroutine("ball", "Ball"));
+    }
+
+    IEnumerator LoadAssetCoroutine(string assetbundleName, string restartTag)
+    {
+        status = string.Format("load assetbundle {0}", assetbundleName);
+        yield return AssetBundle.LoadFromFileAsync(Application.streamingAssetsPath + "/" + assetbundleName);
+        status = "done";
+
+        foreach ( var obj in GameObject.FindGameObjectsWithTag(restartTag))
+        {
+            obj.SetActive(false);
+            obj.SetActive(true);
+        }
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(status);
+    }
+}
---
+++ Assets/Standard Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CreateAssetBundle  {
+
+
+    [MenuItem("Assets/ExportAssetBundle")]
+    static void Export()
+    {
+        System.IO.Directory.CreateDirectory(Application.streamingAssetsPath);
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
